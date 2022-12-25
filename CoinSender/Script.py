32dee1ad7a46280ade7cf41df87a34a4b4bdb0d0
Johulhejn <japtukhina@gmail.com>
2022-12-25 16:05:45 +0300

    Added ERC20_ABI.json

--- CoinSender/Script.py
+++ CoinSender/Script.py
@@ -7,53 +7,8 @@ web3 = Web3(Web3.HTTPProvider(binance_mainnet_rpc_url))
 wallet_address = "0xf035bcd04b399fe35f3f253e801898f5fD4e075d"  # your address
 twt_contract_address = Web3.toChecksumAddress('0x4b0f1812e5df2a09796481ff14017e6005508003')  # TWT token
 
-# only essential functions
-ERC20_ABI = json.loads('''[{
-                            "inputs": [
-                              {
-                                "internalType": "address",
-                                "name": "account",
-                                "type": "address"
-                              }
-                            ],
-                            "name": "balanceOf",
-                            "outputs": [
-                              {
-                                "internalType": "uint256",
-                                "name": "",
-                                "type": "uint256"
-                              }
-                            ],
-                            "stateMutability": "view",
-                            "type": "function"
-                          },
-                          {
-                            "inputs": [
-                              {
-                                "internalType": "address",
-                                "name": "recipient",
-                                "type": "address"
-                              },
-                              {
-                                "internalType": "uint256",
-                                "name": "amount",
-                                "type": "uint256"
-                              }
-                            ],
-                            "name": "transfer",
-                            "outputs": [
-                              {
-                                "internalType": "bool",
-                                "name": "",
-                                "type": "bool"
-                              }
-                            ],
-                            "stateMutability": "nonpayable",
-                            "type": "function"
-                          }
-                        ]
-                ''')
 # initialization of TWT contract
+ERC20_ABI = json.load(open('ERC20_ABI.json'))
 twt_contract = web3.eth.contract(twt_contract_address, abi=ERC20_ABI)
 
 web3.eth.account.enable_unaudited_hdwallet_features()
